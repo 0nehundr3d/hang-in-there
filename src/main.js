829afd2290a97cb20ec1a3743bb3f01e271cd699
Sebastian McKee <sebmcke@gmail.com>
2025-02-03 21:35:52 -0600

    Add form functionality and refactor newPoster method to accept poster objects

--- src/main.js
+++ src/main.js
@@ -12,6 +12,11 @@ const formButton = document.querySelector(".show-form")
 const showMainButton = document.querySelector(".show-main")
 const showSavedButton = document.querySelector(".show-saved")
 const backToMainButton = document.querySelector(".back-to-main")
+
+const posterForm = document.querySelector("form")
+const newPosterImgURL = document.getElementById("poster-image-url")
+const newPosterTitle = document.getElementById("poster-title")
+const newPosterQuote = document.getElementById("poster-quote")
 // we've provided you with some data to work with 👇
 // tip: you can tuck this data out of view with the dropdown found near the line number where the variable is declared 
 var images = [
@@ -115,13 +120,15 @@ var savedPosters = [];
 var currentPoster;
 
 // event listeners go here 👇
-window.addEventListener('load', newPoster)
+window.addEventListener('load', () => { newPoster() })
 
-showRandomButton.addEventListener('click', newPoster)
+showRandomButton.addEventListener('click', () => { newPoster() })
 formButton.addEventListener('click', () => { changeView(mainPosterSection, formSection) })
 showMainButton.addEventListener('click', () => { changeView(formSection, mainPosterSection) })
 showSavedButton.addEventListener('click', () => { changeView(mainPosterSection, savedSection) })
 backToMainButton.addEventListener('click', () => { changeView(savedSection, mainPosterSection) })
+
+posterForm.addEventListener('submit', makePoster)
 // functions and event handlers go here 👇
 // (we've provided two to get you started)!
 function getRandomIndex(array) {
@@ -137,13 +144,43 @@ function createPoster(imageURL, title, quote) {
   }
 }
 
-function newPoster() {
-  posterImage.src = images[getRandomIndex(images)]
-  posterTitle.innerText = titles[getRandomIndex(titles)]
-  posterQuote.innerText = quotes[getRandomIndex(quotes)]
+function randomPoster() {
+  return createPoster(
+    images[getRandomIndex(images)],
+    titles[getRandomIndex(titles)],
+    quotes[getRandomIndex(quotes)]
+  )
+}
+
+function newPoster(poster = null) {
+  if (poster == null) {
+    poster = randomPoster()
+  }
+
+  posterImage.src = poster["imageURL"]
+  posterTitle.innerText = poster["title"]
+  posterQuote.innerText = poster["quote"]
 }
 
 function changeView(hidden, shown) {
   hidden.classList.add("hidden")
   shown.classList.remove("hidden")
+}
+
+function makePoster(event) {
+  event.preventDefault()
+
+  let newUrl = newPosterImgURL.value
+  let newTitle = newPosterTitle.value
+  let newQuote = newPosterQuote.value
+
+  currentPoster = createPoster(newUrl, newTitle, newQuote)
+
+  images.push(newUrl)
+  titles.push(newTitle)
+  quotes.push(newQuote)
+
+  changeView(formSection, mainPosterSection)
+
+  newPoster(currentPoster)
 }
